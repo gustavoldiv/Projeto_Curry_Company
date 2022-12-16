decd6101f2990614ace0c20ec3d92f60e1f46658
Gustavo Lucas <gustavoldiv@gmail.com>
2022-12-16 11:31:37 -0300

    Update 2_Pais.py

--- pages/2_Pais.py
+++ pages/2_Pais.py
@@ -43,31 +43,64 @@ df1 = df1.loc[linhas_selecionadas, :]
 # =======================================
 st.title( 'Visão Países' )
 with st.container():
-    st.markdown( '#### Qtde de Restaurantes por País' )
+        
     df_aux = df1.loc[:,['restaurant_id','pais']].groupby(['pais']).count()
     df_aux = df_aux.sort_values('restaurant_id',ascending=False).reset_index()
-    fig = px.bar(df_aux,x='pais',y='restaurant_id')
-    st.plotly_chart(fig)
     
+    fig = px.bar(df_aux,x='pais',y='restaurant_id'
+                 ,title="Qtde de Restaurantes por País"
+                 ,labels={"pais": "País",
+                          "restaurant_id": "Qtde de Restaurantes"}
+                 ,text_auto=True
+                 ,color_discrete_sequence=['black'])
+    fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    
+    st.plotly_chart(fig)
+        
 with st.container():
-    st.markdown( '#### Qtde de Cidades por País' )
+    
     df_aux = df1.loc[:,['city','pais']].groupby(['pais']).nunique()
     df_aux = df_aux.sort_values('city',ascending=False).reset_index()
-    fig = px.bar(df_aux,x='pais',y='city')
+    
+    fig = px.bar(df_aux,x='pais',y='city'
+                 ,title="Qtde de Cidades por País"
+                 ,labels={"pais": "País",
+                          "city": "Cidade"}
+                 ,text_auto=True
+                 ,color_discrete_sequence=['red'])
+    fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    
     st.plotly_chart(fig)
     
 with st.container():
     col1,col2 = st.columns( 2, gap='large' )
     with col1:
-        st.markdown( '#### Média de avaliações feitas por país' )
-        df_aux = df1.loc[:,['aggregate_rating','pais']].groupby(['pais']).mean()
+        
+        df_aux = round(df1.loc[:,['aggregate_rating','pais']].groupby(['pais']).mean(),2)
         df_aux = df_aux.sort_values('aggregate_rating',ascending=False).reset_index()
-        fig = px.bar(df_aux,x='pais',y='aggregate_rating')
+        
+        fig = px.bar(df_aux,x='pais',y='aggregate_rating'
+                 ,title="Média de avaliação por País"
+                 ,labels={"pais": "País",
+                          "aggregate_rating": "Média de avaliação"}
+                 ,text_auto=True
+                 ,color_discrete_sequence=['gold'])
+        fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        
         st.plotly_chart(fig)
         
     with col2:
-        st.markdown( '#### Média de um prato para 2 pessoas por país' )
-        df_aux = df1.loc[:,['average_cost_for_two','pais']].groupby(['pais']).mean()
+        
+        df_aux = round(df1.loc[:,['average_cost_for_two','pais']].groupby(['pais']).mean(),2)
         df_aux = df_aux.sort_values('average_cost_for_two',ascending=False).reset_index()
+        
         fig = px.bar(df_aux,x='pais',y='average_cost_for_two')
+        fig = px.bar(df_aux,x='pais',y='average_cost_for_two'
+                 ,title="Média de um prato para 2 pessoas por País"
+                 ,labels={"pais": "País",
+                          "average_cost_for_two": "Média de 1 prato para 2 pessoas"}
+                 ,text_auto=True
+                 ,color_discrete_sequence=['orange'])
+        fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        
         st.plotly_chart(fig)
